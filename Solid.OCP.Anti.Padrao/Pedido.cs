115f9ba7007224c5358f3ced7a877724901ef04f
Juliano Ribeiro de Souza Maciel <juliano.maciel@totvs.com.br>
2021-04-28 20:29:18 -0300

    Inclusão do OCP

---
+++ Solid.OCP.Anti.Padrao/Pedido.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.OCP.Anti.Padrao
+{
+    public class Pedido
+    {
+    }
+}
---
+++ Solid.OCP.Anti.Padrao/Repositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.OCP.Anti.Padrao
+{
+    public class Repositorio
+    {
+
+        public void Salvar()
+        {
+
+        }
+
+        public void Remover()
+        {
+
+        }
+    }
+}
